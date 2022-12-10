c6fe03d374e500f090e8bb755a70c8450eca4aed
richardjzhang <richardjzhang@outlook.com>
2022-12-10 21:26:26 +1100

    Add revalidation

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,7 @@ export async function getStaticProps() {
     props: {
       posts: data,
     },
+    revalidate: 60,
   };
 }
 
--- utils/database.ts
+++ utils/database.ts
@@ -3,14 +3,14 @@ import PostModel from "models/post";
 
 export const fetchAllPosts = async () => {
   await dbConnect();
-  const posts = await PostModel.find({});
+  const posts = await PostModel.find({}).exec();
   return JSON.parse(JSON.stringify(posts));
 };
 
 export const fetchPost = async (id: string) => {
   await dbConnect();
-  const post = await PostModel.findOne({}, { _id: id }).select(
-    "_id title description slug image createdAt"
-  );
+  const post = await PostModel.findOne({ _id: id })
+    .select("_id title description slug image createdAt")
+    .exec();
   return JSON.parse(JSON.stringify(post));
 };
